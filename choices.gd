72a7b480af617de06b35202090b3a741350b020c
Simon Humphrey <snhumphr@ualberta.ca>
2024-02-17 14:42:17 -0700

    made the event thingy display the name of the event at the top, as a title

--- choices.gd
+++ choices.gd
@@ -3,7 +3,10 @@ extends PanelContainer
 func init(event: Event):
 	
 	var description = self.get_node("layout/description")
-	description.append_text(event.description)
+	
+	var title = "[center]" + "[b]" + event.name + "[/b]" + "[/center]" + "\n" + "\n"
+	description.append_text(title)
+	description.append_text("[p]" + event.description + "[/p]")
 	
 	var choices = self.get_node("layout/choices")
 	
